ba2057ef7ae2138ab75ce81ade6f83a86ca43c49
Kazuya Hatano <k28@me.com>
2021-05-08 07:23:33 +0900

    initial commit

---
+++ GatherQR/GatherQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatherQRApp.swift
+//  GatherQR
+//
+//  Created by K.Hatano on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct GatherQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
